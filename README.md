6565d49d50591dda5256d39184a5ea42c15c9b78
Trinity Cisneros <trinitycisneros@Trinitys-MacBook-Pro.local>
2019-05-09 22:50:53 -0700

    final changes

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 ![Newsroom](https://media.giphy.com/media/v2xIous7mnEYg/giphy.gif)
 
 ## Background
+The relationship between obesity and poverty is constantly discussed in the context of food islands, and poor health prognosis in low income communities.  The goal of this project is to evaluate the association between poverty (independent variable)  and obseity (dependent variable), using 2014 census data.
 
-Welcome to the newsroom! You've just accepted a data visualization position for a major metro paper. You're tasked with analyzing the current trends shaping people's lives, as well as creating charts, graphs, and interactive elements to help readers understand your findings.
-
-The editor wants to run a series of feature stories about the health risks facing particular demographics. She's counting on you to sniff out the first story idea by sifting through information from the U.S. Census Bureau and the Behavioral Risk Factor Surveillance System.
-
-The data set included with the assignment is based on 2014 ACS 1-year estimates: [https://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml](https://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml), but you are free to investigate a different data set. The current data set incldes data on rates of income, obesity, poverty, etc. by state. MOE stands for "margin of error."
+To run this D3 visualization, open the html file. The data, js, and accompanying css files are used to prepare this analysis.
 
 ## Your Task
 
@@ -28,42 +25,4 @@ Using the D3 techniques we taught you in class, create a scatter plot that repre
 
 - - -
 
-### Level 2: Impress the Boss (Optional Challenge Assignment)
 
-Why make a static graphic when D3 lets you interact with your data?
-
-![7-animated-scatter](Images/7-animated-scatter.gif)
-
-#### 1. More Data, More Dynamics
-
-You're going to include more demographics and more risk factors. Place additional labels in your scatter plot and give them click events so that your users can decide which data to display. Animate the transitions for your circles' locations as well as the range of your axes. Do this for two risk factors for each axis. Or, for an extreme challenge, create three for each axis.
-
-* Hint: Try binding all of the CSV data to your circles. This will let you easily determine their x or y values when you click the labels.
-
-#### 2. Incorporate d3-tip
-
-While the ticks on the axes allow us to infer approximate values for each circle, it's impossible to determine the true value without adding another layer of data. Enter tooltips: developers can implement these in their D3 graphics to reveal a specific element's data when the user hovers their cursor over the element. Add tooltips to your circles and display each tooltip with the data that the user has selected. Use the `d3-tip.js` plugin developed by [Justin Palmer](https://github.com/Caged)—we've already included this plugin in your assignment directory.
-
-![8-tooltip](Images/8-tooltip.gif)
-
-* Check out [David Gotz's example](https://bl.ocks.org/davegotz/bd54b56723c154d25eedde6504d30ad7) to see how you should implement tooltips with d3-tip.
-
-- - -
-
-### Assessment
-
-Your final product will be assessed on the following metrics:
-
-* Completion of all steps in chosen level
-
-* Coherency of scatter plot (labels, ticks)
-
-* Visual attraction
-
-* Professionalism
-
-**Good luck!**
-
-## Copyright
-
-Data Boot Camp © 2018. All Rights Reserved.
--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,138 @@
-// @TODO: YOUR CODE HERE!
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from csv
+d3.csv("assets/data/data.csv").then(function(data) {
+
+
+    // step 1: parse data cast as numbers
+    data.forEach(function(d) {
+      d.poverty = +d.poverty;
+      d.povertyMoe = +d.povertyMoe;
+      d.age = +d.age;
+      d.ageMoe = +d.ageMoe;
+      d.income = +d.income;
+      d.incomeMoe = +d.incomeMoe;
+      d.healthcare = +d.healthcare;
+      d.healthcareHigh = +d.healthcareHigh;
+      d.obesity = +d.obesity;
+      d.obesityLow = +d.obesityLow;
+      d.obesityHigh = +d.obesityHigh;
+      d.smokes = +d.smokes;
+      d.smokesLow = +d.smokesLow;
+      d.smokesHigh = +d.smokesHigh;
+      });
+
+      // var xExtent = d3.extent(data, d => d.poverty)
+      // var yExtent = d3.extent(data, d => d.obesity)
+
+      var xScale = d3.scaleLinear()
+        // .domain(xExtent)
+        .domain([0, d3.max(data, d => d.poverty)])
+        .range([0, chartWidth]);
+      
+      var yScale = d3.scaleLinear()
+      // .domain(yExtent)
+        .domain([0, d3.max(data, d => d.obesity)])
+        .range([chartHeight, 0]);
+
+      // Step 3: create axis functions
+      var xAxis = d3.axisBottom(xScale);
+      var yAxis = d3.axisLeft(yScale);
+
+
+      // Step 4: Append Axes to the chart 
+      chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(xAxis);
+  
+      chartGroup.append("g")
+      .call(yAxis);
+
+      var circleGroup = chartGroup.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .classed("stateCircle", true)
+      .attr("cx", d => xScale(d.poverty))
+      .attr("cy", d => yScale(d.obesity))
+      .attr("r", "15")
+      .attr("fill", "blue")
+      .attr("opacity", ".5");
+      
+      // Create axes labels
+      chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - chartMargin.left)
+      .attr("x", 0 - (chartHeight / 2))
+      .attr("dy", "1em")
+      .classed("axis-text", true)
+      .text("Obesity");
+
+      chartGroup.append("text")
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Poverty");
+      
+      
+      var circRadius = 15;
+      
+      // Add state abbr to each plot
+      var stateLabels = chartGroup.selectAll("text.stateText")
+      .data(data)
+      .enter()
+      .append("text")
+      .classed("stateText", true)
+      .attr("x", d => xScale(d.poverty))
+      .attr("y", d => yScale(d.obesity))
+      .attr("font-size", circRadius)
+      .text(d => d.abbr);
+      
+      
+      // var circleGroup = chartGroup.append("g")
+      //   .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+      // // theCircles.append("text")
+      // circleGroup
+      // .append("text")
+      // // We return the abbreviation to .text, which makes the text the abbreviation.
+      // .text(function(data) {
+      //   return data.abbr;
+      // })
+      // // Now place the text using our scale.
+      // .attr("dx", function(data) {
+      //   return xScale(data.poverty);
+      // })
+      // .attr("dy", function(data) {
+      //   // When the size of the text is the radius,
+      //   // adding a third of the radius to the height
+      //   // pushes it into the middle of the circle.
+      //   return yScale(data.obesity) + circRadius / 2.5;
+      // })
+      // .attr("font-size", circRadius)
+      // .attr("class", "stateText");
+});
